075aea5cd89ac12921c297e825a08adc67e57f2f
Pablo Vergara <vergara.pablo0616@gmail.com>
2023-06-07 18:23:45 -0400

    tests for global header

--- tests/sauceDemo/units/test_sauceDemo_globalHeader.py
+++ tests/sauceDemo/units/test_sauceDemo_globalHeader.py
@@ -6,7 +6,6 @@ from sauceUtils.data import SauceDemoData
 from sauceModels.saucedemo_login import LoginPage
 
 
-@pytest.mark.unit_test
 @pytest.fixture(scope="function", autouse=True)
 def before_each(page: Page):
     page.goto(SauceDemoData.sauceURL)
@@ -15,37 +14,51 @@ def before_each(page: Page):
     onLoginPage.submitLogin(SauceDemoData.validUSN, SauceDemoData.password)
     yield
     
-    #TODO repurpose login authentication
+    #TODO repurpose login authentication .. getting a 405 at the moment
 
+@pytest.mark.unitTest
 def test_productsPage_has_url(page: Page):
     expect(page).to_have_url(re.compile(SauceDemoData.productsURL))
-    
+
+@pytest.mark.unitTest   
 def test_productsPageHeader_has_title(page: Page):
     expect(page).to_have_title(re.compile(SauceDemoData.pageTitle))
 
-#GLOBAL HEADER LOGO    
+#GLOBAL HEADER LOGO
+@pytest.mark.unitTest    
 def test_productsPageHeader_has_logo(page: Page):
     expect(page.locator(SwagLabsHeaderLocators.HEADER_LOGO)).to_be_visible()
 
 #GLOBAL HEADER MENU
+@pytest.mark.unitTest
 def test_productsPageHeader_has_menu_button(page: Page):
     expect(page.locator(SwagLabsHeaderLocators.MENU_BUTTON)).to_be_visible()
-        
+
+@pytest.mark.unitTest        
 def test_productsPageHeader_menu_button_has_id(page: Page, element_id="react-burger-menu-btn"):
     expect(page.locator(SwagLabsHeaderLocators.MENU_BUTTON)).to_have_id(element_id)
-    
+
+@pytest.mark.unitTest    
 def test_productsPageHeader_menu_button_is_actionable(page: Page):
     expect(page.locator(SwagLabsHeaderLocators.MENU_BUTTON)).not_to_be_disabled()
 
+@pytest.mark.unitTest
 def test_productsPageHeader_menu_button_not_immediately_focused_when_page_is_accessed(page: Page):
     expect(page.locator(SwagLabsHeaderLocators.MENU_BUTTON)).not_to_be_focused()
     
 #GLOBAL HEADER CART BUTTON
+@pytest.mark.unitTest
 def test_productsPageHeader_has_cart_button(page: Page):
     expect(page.locator(SwagLabsHeaderLocators.CART_BUTTON)).to_be_visible()
     
+@pytest.mark.unitTest
 def test_productsPageHeader_cart_button_has_id(page: Page, element_id="shopping_cart_container"):
     expect(page.locator(SwagLabsHeaderLocators.CART_BUTTON)).to_have_id(element_id)
     
+@pytest.mark.unitTest
 def test_productsPageHeader_cart_button_is_actionable(page: Page):
     expect(page.locator(SwagLabsHeaderLocators.CART_BUTTON)).not_to_be_disabled()
+    
+@pytest.mark.unitTest
+def test_productsPageHeader_cart_button_item_added_badge_is_not_displayed(page: Page):
+    expect(page.locator(SwagLabsHeaderLocators.ITEM_ADDED_BADGE)).not_to_be_visible()
--- tests/sauceDemo/units/test_sauceDemo_loginForm.py
+++ tests/sauceDemo/units/test_sauceDemo_loginForm.py
@@ -5,7 +5,6 @@ from sauceLocators.page_elements import *
 from sauceUtils.data import SauceDemoData
 
 
-@pytest.mark.unit_test
 @pytest.fixture(scope="function", autouse=True)
 #use --browser-channel "chrome" to run tests in chrome, not chromium
 
@@ -14,53 +13,69 @@ def before_each(page: Page):
     yield
 
 #LOGIN PAGE    
+@pytest.mark.unitTest
 def test_loginForm_has_url(page: Page):
     expect(page).to_have_url(re.compile(SauceDemoData.sauceURL))
     
+@pytest.mark.unitTest
 def test_loginForm_has_title(page: Page):
     expect(page).to_have_title(re.compile(SauceDemoData.pageTitle))
     
+@pytest.mark.unitTest
 def test_loginForm_has_logo(page: Page):
     expect(page.locator(SauceDemoPageLocators.SAUCE_LOGO)).to_be_visible()      
 
 #USERNAME INPUT
+@pytest.mark.unitTest
 def test_loginForm_username_input_is_visible(page: Page):
     expect(page.locator(SauceDemoPageLocators.USN_INPUT)).to_be_visible()
 
+@pytest.mark.unitTest
 def test_loginForm_username_input_is_not_populated(page: Page):
     expect(page.locator(SauceDemoPageLocators.USN_INPUT)).to_be_empty()
     
+@pytest.mark.unitTest
 def test_loginForm_username_input_is_editable(page: Page):
     expect(page.locator(SauceDemoPageLocators.USN_INPUT)).to_be_editable()
     
+@pytest.mark.unitTest
 def test_loginForm_username_input_has_id(page: Page, element_id="user-name"):
     expect(page.locator(SauceDemoPageLocators.USN_INPUT)).to_have_id(element_id)
 
+@pytest.mark.unitTest
 def test_loginForm_username_input_has_hint_text(page: Page):
     expect(page.locator(SauceDemoPageLocators.USN_INPUT)).to_have_attribute("placeholder", "Username")
 
 #PASSWORD INPUT
+@pytest.mark.unitTest
 def test_loginForm_password_input_is_visible(page: Page):
     expect(page.locator(SauceDemoPageLocators.PWD_INPUT)).to_be_visible()
 
+@pytest.mark.unitTest
 def test_loginForm_password_input_is_not_populated(page: Page):
     expect(page.locator(SauceDemoPageLocators.PWD_INPUT)).to_be_empty()
     
+@pytest.mark.unitTest
 def test_loginForm_password_input_is_editable(page: Page):
     expect(page.locator(SauceDemoPageLocators.PWD_INPUT)).to_be_editable()
     
+@pytest.mark.unitTest
 def test_loginForm_password_input_has_id(page: Page, element_id="password"):
     expect(page.locator(SauceDemoPageLocators.PWD_INPUT)).to_have_id(element_id)
 
+@pytest.mark.unitTest
 def test_loginForm_password_input_has_hint_text(page: Page):
     expect(page.locator(SauceDemoPageLocators.PWD_INPUT)).to_have_attribute("placeholder", "Password")
     
 #LOGIN BUTTON
+@pytest.mark.unitTest
 def test_loginForm_submit_button_is_displayed(page: Page):
     expect(page.locator(SauceDemoPageLocators.SUBMIT_BTN)).to_be_visible()
 
+@pytest.mark.unitTest
 def test_loginForm_submit_button_is_actionable(page: Page):
     expect(page.locator(SauceDemoPageLocators.SUBMIT_BTN)).not_to_be_disabled()     
 
+@pytest.mark.unitTest
 def test_loginForm_submit_button_has_value(page: Page):
     expect(page.locator(SauceDemoPageLocators.SUBMIT_BTN)).to_have_value("Login")
