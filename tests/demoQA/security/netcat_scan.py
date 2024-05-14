30e983f51331295744cbda826b03853216cb53b6
Pablo Vergara <vergara.pablo0616@gmail.com>
2024-05-14 17:40:11 -0400

    added banner grap function

--- tests/demoQA/security/netcat_scan.py
+++ tests/demoQA/security/netcat_scan.py
@@ -9,7 +9,15 @@ RECON STEP: NETWORK SCANNER w. NET CAT
 
 @pytest.mark.security
 def scan_ip(ip):
+    """Fingerprint Webserver"""
     os.system(f"nc -nv -w 1 -z {ip} 80")
     
     
-scan_ip(input("Please enter the target IP address: "))
+def banner_grab(ip):
+    """Banner grab - verbose option"""
+    os.system(f"nc nv {ip} 80")
+    
+    
+target_ip = input("Please enter the target IP address: ")    
+scan_ip(target_ip)
+# banner_grab(target_ip)
